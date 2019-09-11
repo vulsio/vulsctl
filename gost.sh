92536e7fe89c00f2ade35a85d9f3852ed57ac478
Kota Kanbe <kotakanbe@gmail.com>
2019-09-11 12:05:07 +0900

    fix gost.sh

--- gost.sh
+++ gost.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-docker run --rm -i \
-    -v $PWD:/vuls \
-    vuls/gost fetch redhat
+if [ $# -ne 1 ]; then
+  echo "specify [redhat debian] as cmd arg"
+  exit 1
+fi
+
+if [ $1 = "redhat" ]; then
+    docker run --rm -i \
+        -v $PWD:/vuls \
+        vuls/gost fetch redhat
+fi
+
+if [ $1 = "debian" ]; then
+    docker run --rm -i \
+        -v $PWD:/vuls \
+        vuls/gost fetch debian
+fi
