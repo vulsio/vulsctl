0692119a55f0e9736a83219a8b9ac1c793330136
Kota Kanbe <kotakanbe@gmail.com>
2019-09-27 12:52:37 +0900

    fix

--- gost.sh
+++ gost.sh
@@ -1,32 +1,31 @@
 #! /bin/sh -
 
-docker pull vuls/gost
-
-if [ $# -ne 1 ]; then
+if [ $# -eq 0 ]; then
 	echo "specify [--redhat --debian]"
 	exit 1
 fi
 
-while test $# -gt 0
-do
-	case "$1" in
-		--redhat) docker run --rm -it \
-			-v $PWD:/vuls \
-			vuls/gost fetch redhat
-			;;
-		--debian) docker run --rm -it \
-			-v $PWD:/vuls \
-			vuls/gost fetch debian
-			;;
-		--*)  echo "specify [--redhat --debian]"
-			exit 1
-		    ;;
-		*) echo "specify [--redhat --debian]"
-			exit 1
-		    ;;
-	esac
-	shift
-done
+target=$1
+shift
+
+docker pull vuls/gost
+
+case "$target" in
+	--redhat) docker run --rm -it \
+		-v ${PWD}:/vuls \
+		vuls/gost fetch ${@} redhat
+		;;
+	--debian) docker run --rm -it \
+		-v ${PWD}:/vuls \
+		vuls/gost fetch ${@} debian
+		;;
+	--*)  echo "specify [--redhat --debian]"
+		exit 1
+	    ;;
+	*) echo "specify [--redhat --debian]"
+		exit 1
+	    ;;
+esac
 
 exit 0
 
--- oval.sh
+++ oval.sh
@@ -1,44 +1,43 @@
 #! /bin/sh -
 
-docker pull vuls/goval-dictionary
-
-if [ $# -ne 1 ]; then
+if [ $# -eq 0 ]; then
 	echo "specify [--redhat --amazon --debian --ubuntu --alpine]"
 	exit 1
 fi
 
-while test $# -gt 0
-do
-	case "$1" in
-		--redhat) docker run --rm -it \
-			-v $PWD:/vuls \
-			vuls/goval-dictionary fetch-redhat  6 7 8 
-			;;
-		--amazon) docker run --rm -it \
-			-v $PWD:/vuls \
-			vuls/goval-dictionary fetch-amazon
-			;;
-		--debian) docker run --rm -it \
-			-v $PWD:/vuls \
-			vuls/goval-dictionary fetch-debian 8 9 10
-			;;
-		--ubuntu) docker run --rm -it \
-			-v $PWD:/vuls \
-			vuls/goval-dictionary fetch-ubuntu 16 18 
-			;;
-		--alpine) docker run --rm -it \
-			-v $PWD:/vuls \
-			vuls/goval-dictionary fetch-alpine 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10
-			;;
-		--*)  echo "specify [--redhat --amazon --debian --ubuntu --alpine]"
-			exit 1
-			;;
-		*) echo "specify [--redhat --amazon --debian --ubuntu --alpine]"
-			exit 1
-			;;
-	esac
-	shift
-done
+target=$1
+shift
+
+docker pull vuls/goval-dictionary
+
+case "$target" in
+	--redhat) docker run --rm -it \
+		-v $PWD:/vuls \
+		vuls/goval-dictionary fetch-redhat ${@} 6 7 8 
+		;;
+	--amazon) docker run --rm -it \
+		-v $PWD:/vuls \
+		vuls/goval-dictionary fetch-amazon ${@} 
+		;;
+	--debian) docker run --rm -it \
+		-v $PWD:/vuls \
+		vuls/goval-dictionary fetch-debian ${@} 8 9 10
+		;;
+	--ubuntu) docker run --rm -it \
+		-v $PWD:/vuls \
+		vuls/goval-dictionary fetch-ubuntu ${@} 16 18 
+		;;
+	--alpine) docker run --rm -it \
+		-v $PWD:/vuls \
+		vuls/goval-dictionary fetch-alpine ${@} 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10
+		;;
+	--*)  echo "specify [--redhat --amazon --debian --ubuntu --alpine]"
+		exit 1
+		;;
+	*) echo "specify [--redhat --amazon --debian --ubuntu --alpine]"
+		exit 1
+		;;
+esac
 
 exit 0
 
