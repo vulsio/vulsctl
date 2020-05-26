07db978ee29696d25db44b44ed323c1a3e367422
Jonathan Barda <jonathan.barda@gmail.com>
2020-05-27 01:21:16 +0200

    Added support for RHEL / CentOS 6.x and 7.x
    Also fixed the `OS` detection code

--- install-host.sh
+++ install-host.sh
@@ -68,6 +68,67 @@ ubuntu() {
 	echo "Done."; 
 }
 
+centos() {
+	echo -e "$RED""Finding latest version of Go for AMD64...""$NC";
+	url="$(wget -qO- https://golang.org/dl/ | grep -oP 'https:\/\/dl\.google\.com\/go\/go([0-9\.]+)\.linux-amd64\.tar\.gz' | head -n 1 )";
+	latest="$(echo $url | grep -oP 'go[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2 )";
+	wget "${url}";
+	echo -e "$RED""[!] Download successful : $url""$NC";
+	tar -C /usr/local -xzf go$latest.linux-amd64.tar.gz;
+	mkdir $HOME/go;
+	export GOROOT=/usr/local/go;
+	export GOPATH=$HOME/go;
+	export PATH=$PATH:$GOROOT/bin:$GOPATH/bin;
+	echo "export GOROOT=/usr/local/go" >> "$HOME"/.profile;
+	echo "export GOPATH=$HOME/go" >> "$HOME"/.profile;
+	echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> "$HOME"/.profile;
+	echo -e "$RED""go-cve-dictionary + goval-dictionary installing...""$NC";
+	mkdir /var/log/vuls;
+	chown $ID /var/log/vuls
+	chmod 700 /var/log/vuls
+	mkdir -p $GOPATH/src/github.com/kotakanbe;
+	cd $GOPATH/src/github.com/kotakanbe;
+	git clone https://github.com/kotakanbe/go-cve-dictionary.git;
+	git clone https://github.com/kotakanbe/goval-dictionary.git;
+	cd $GOPATH/src/github.com/kotakanbe/go-cve-dictionary; 
+	make install;
+	cd $GOPATH/src/github.com/kotakanbe/goval-dictionary;
+	make install;
+	ln -s $GOPATH/src/github.com/kotakanbe/goval-dictionary/oval.sqlite3 $HOME/oval.sqlite3;
+	echo -e "$RED""gost(go-security-tracker) installing...""$NC";
+	mkdir /var/log/gost
+	chown $ID /var/log/gost;
+	chmod 700 /var/log/gost;
+	mkdir -p $GOPATH/src/github.com/knqyf263;
+	cd $GOPATH/src/github.com/knqyf263;
+	git clone https://github.com/knqyf263/gost.git;
+	cd gost;
+	make install;
+	ln -s $GOPATH/src/github.com/knqyf263/gost/gost.sqlite3 $HOME/gost.sqlite3;
+	echo -e "$RED""go-exploitdb installing...""$NC";	
+	mkdir /var/log/go-exploitdb
+	chown $ID /var/log/go-exploitdb
+	chmod 700 /var/log/go-exploitdb
+	mkdir -p $GOPATH/src/github.com/mozqnet;
+	cd $GOPATH/src/github.com/mozqnet;
+	git clone https://github.com/mozqnet/go-exploitdb.git;
+	cd go-exploitdb;
+	make install;
+	ln -s $GOPATH/src/github.com/mozqnet/go-exploitdb/go-exploitdb.sqlite3 $HOME/go-exploitdb.sqlite3;
+	echo -e "$RED""Vuls installing...""$NC";
+	mkdir -p $GOPATH/src/github.com/future-architect;
+	cd $GOPATH/src/github.com/future-architect;
+	git clone https://github.com/future-architect/vuls.git;
+	cd vuls;
+	make install; 
+	cp $GOPATH/bin/go-cve-dictionary /usr/local/bin/
+	cp $GOPATH/bin/go-exploitdb /usr/local/bin/
+	cp $GOPATH/bin/gost /usr/local/bin/
+	cp $GOPATH/bin/goval-dictionary /usr/local/bin/
+	cp $GOPATH/bin/vuls /usr/local/bin/
+	echo "Done."; 
+}
+
 # https://github.com/namhyung/uftrace/blob/master/misc/install-deps.sh
 if [ "x$(id -u)" != x0 ]; then
 	echo "You might have to run it as root user."
@@ -79,11 +140,18 @@ fi
 OPT="${@}"
 
 distro=$(grep "^ID=" /etc/os-release | cut -d\= -f2 | sed -e 's/"//g')
+if [[ $distro == "" ]]; then
+    # Use the other way to detect the OS for CentOS 6.x
+    distro=$(cat /etc/redhat-release | awk '{print tolower($1)}')
+fi
 
 case $distro in
 	"ubuntu")
 		apt-get $OPT install sqlite git gcc make wget
 		ubuntu;;
+	"rhel" | "centos")
+		yum $OPT install sqlite git gcc make wget
+		centos;;
 	*) # we can add more install command for each distros.
 		echo "\"$distro\" is not supported distro, so please install packages manually." ;;
 esac
