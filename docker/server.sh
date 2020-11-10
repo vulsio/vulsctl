27125dbcfce1c8c890968263aa21b781b0b9bec6
Jonathan Barda <jonathan.barda@pm.me>
2020-11-10 21:07:47 +0100

    Removed extra lines and cleaned paths
    To reflect the instruction given [here](https://vuls.io/docs/en/tutorial-vulsctl-docker.html), the extra lines had been removed.

--- docker/server.sh
+++ docker/server.sh
@@ -6,7 +6,7 @@ docker run --rm -it\
     -v $HOME/.ssh:/root/.ssh:ro \
     -v $PWD:/vuls \
     vuls/vuls configtest \
-    -log-dir=/vuls/docker/log \
+    -log-dir=/vuls/log \
     -config=/vuls/config.toml \
     $@
 
@@ -21,13 +21,8 @@ docker run --rm -it\
     -p 5515:5515 \
     vuls/vuls server \
     -listen=0.0.0.0:5515 \
-    -cvedb-sqlite3-path=/vuls/docker/cve.sqlite3 \
-    -ovaldb-sqlite3-path=/vuls/docker/oval.sqlite3 \
-    -gostdb-sqlite3-path=/vuls/docker/gost.sqlite3 \
-    -exploitdb-sqlite3-path=/vuls/docker/go-exploitdb.sqlite3 \
-    -msfdb-sqlite3-path=/vuls/docker/go-msfdb.sqlite3 \
     -results-dir=/vuls/results \
-    -log-dir=/vuls/docker/log \
+    -log-dir=/vuls/log \
     -config=/vuls/config.toml \
     -to-localfile \
     -format-json \
