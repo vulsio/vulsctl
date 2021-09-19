76b5a1c2aeab04db23fecc11817256014c2ab63a
Kota Kanbe <kotakanbe@gmail.com>
2021-09-20 04:59:38 +0900

    fix for Vuls v0.18.0
    https://github.com/future-architect/vuls/pull/1307

--- docker/update-all-redis.sh
+++ docker/update-all-redis.sh
@@ -4,16 +4,16 @@ export DOCKER_NETWORK=redis-nw
 redis_host='redis'
 redis_url="redis://${redis_host}"
 
-./oval.sh --redhat --dbtype redis --dbpath ${redis_url}/1 --debug ${@} 
-./oval.sh --amazon --dbtype redis --dbpath ${redis_url}/1 --debug ${@}
-./oval.sh --debian --dbtype redis --dbpath ${redis_url}/1 --debug ${@} 
-./oval.sh --ubuntu --dbtype redis --dbpath ${redis_url}/1 --debug ${@} 
-./oval.sh --alpine --dbtype redis --dbpath ${redis_url}/1 --debug ${@}
-./oval.sh --oracle --dbtype redis --dbpath ${redis_url}/1 --debug ${@}
-./gost.sh --redhat --dbtype redis --dbpath ${redis_url}/2 --debug ${@} 
-./gost.sh --debian --dbtype redis --dbpath ${redis_url}/2 --debug ${@} 
-./gost.sh --ubuntu --dbtype redis --dbpath ${redis_url}/2 --debug ${@} 
-./nvd.sh --dbtype redis --dbpath ${redis_url}/3 --debug ${@} 
-./jvn.sh --dbtype redis --dbpath ${redis_url}/3 --debug ${@} 
-./exploitdb.sh --dbtype redis --dbpath ${redis_url}/4 --debug ${@}
-./msfdb.sh --dbtype redis --dbpath ${redis_url}/5 --debug ${@} 
+./oval.sh --redhat --dbtype redis --dbpath ${redis_url}/1 ${@} 
+./oval.sh --amazon --dbtype redis --dbpath ${redis_url}/1 ${@}
+./oval.sh --debian --dbtype redis --dbpath ${redis_url}/1 ${@} 
+./oval.sh --ubuntu --dbtype redis --dbpath ${redis_url}/1 ${@} 
+./oval.sh --alpine --dbtype redis --dbpath ${redis_url}/1 ${@}
+./oval.sh --oracle --dbtype redis --dbpath ${redis_url}/1 ${@}
+./gost.sh --redhat --dbtype redis --dbpath ${redis_url}/2 ${@} 
+./gost.sh --debian --dbtype redis --dbpath ${redis_url}/2 ${@} 
+./gost.sh --ubuntu --dbtype redis --dbpath ${redis_url}/2 ${@} 
+./nvd.sh --dbtype redis --dbpath ${redis_url}/3 ${@} 
+./jvn.sh --dbtype redis --dbpath ${redis_url}/3 ${@} 
+./exploitdb.sh --dbtype redis --dbpath ${redis_url}/4 ${@}
+./msfdb.sh --dbtype redis --dbpath ${redis_url}/5 ${@} 
--- install-host/cvedb.sh
+++ install-host/cvedb.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
 
-RELEASE=v0.13.1
-URL=https://github.com/future-architect/vuls/releases/download/${RELEASE}/cve.sqlite3.gz 
-
-if [ ! -e ./cve.sqlite3 ]; then
-    echo "Fetching cve.sqlite3 from GitHub Vuls: ${URL}"
-    curl -OL ${URL}
-    gunzip ./cve.sqlite3.gz
-fi
-
-for i in `seq 2002 $(date +"%Y")`; do \
-    go-cve-dictionary fetch nvd $@ --years $i; \
-done
-
-for i in `seq 1998 $(date +"%Y")`; do \
-    go-cve-dictionary fetch jvn $@ --years $i; \
-done
+go-cve-dictionary fetch nvd $@
+go-cve-dictionary fetch jvn $@
