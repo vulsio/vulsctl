ad5067b595cb7d2d88a19d5415f8aa6242019ce6
Shane McCarron <halindrome@gmail.com>
2021-12-20 08:25:43 -0600

    added tty check to kev.sh

--- docker/kev.sh
+++ docker/kev.sh
@@ -3,15 +3,21 @@
 docker pull vuls/go-kev
 
 if [[ -z "${DOCKER_NETWORK}" ]]; then
-	DOCKER_NETWORK_OPT=""
+        DOCKER_NETWORK_OPT=""
 else
-	DOCKER_NETWORK_OPT="--network ${DOCKER_NETWORK}"
+        DOCKER_NETWORK_OPT="--network ${DOCKER_NETWORK}"
 fi
 
-docker run --rm -it vuls/go-kev version
+if [[ $(tty) =~ "not a tty" ]]
+then
+    t=''
+else
+    t="-t"
+fi
 
-docker run --rm -it \
+docker run --rm -i $t vuls/go-kev version
+
+docker run --rm -i $t \
     ${DOCKER_NETWORK_OPT} \
     -v $PWD:/go-kev \
     vuls/go-kev fetch kevuln ${@}
-
